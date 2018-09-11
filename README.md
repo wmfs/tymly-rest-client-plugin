d1fa052b2bc86632b69c320734ec33c1761620d0
Jez Higgins <jez@jezuk.co.uk>
2018-09-11 12:03:31 +0100

    feat: REST API client state resource
    Pulled out of tymly-express-plugin into its own package

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tymly-rest-client-plugin
-REST API state resources
+[![Tymly Plugin](https://img.shields.io/badge/tymly-plugin-blue.svg)](https://tymly.io/)
+[![npm (scoped)](https://img.shields.io/npm/v/@wmfs/tymly-rest-client-plugin.svg)](https://www.npmjs.com/package/@wmfs/tymly-rest-client-plugin)
+[![Build Status](https://travis-ci.org/wmfs/tymly-rest-client-plugin.svg?branch=master)](https://travis-ci.org/wmfs/tymly-rest-client-plugin)
+[![codecov](https://codecov.io/gh/wmfs/tymly-rest-client-plugin/branch/master/graph/badge.svg)](https://codecov.io/gh/wmfs/tymly-rest-client-plugin)
+[![CodeFactor](https://www.codefactor.io/repository/github/wmfs/tymly-rest-client-plugin/badge)](https://www.codefactor.io/repository/github/wmfs/tymly-rest-client-plugin)
+[![Dependabot badge](https://img.shields.io/badge/Dependabot-active-brightgreen.svg)](https://dependabot.com/)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/wmfs/tymly-rest-client-plugin/blob/master/LICENSE)
+
+> REST API state resource for [TymlyJS](http://www.tymlyjs.io)
+
+# <a name="license"></a>License
+
+MIT
---
+++ test/get-rest-api-test.js
@@ -0,0 +1,120 @@
+/* eslint-env mocha */
+
+const expect = require('chai').expect
+const express = require('express')
+const RestApiClient = require('./../lib/components/state-resources/get-data-from-rest-api')
+
+const mockRegistry = {
+  keys: {
+    'test_url': 'http://localhost:3003/test-endpoint',
+    'test_nocontent': 'http://localhost:3003/no-content',
+    'test_notfound': 'http://localhost:3003/not-found',
+    'test_servererror': 'http://localhost:3003/server-error'
+  },
+
+  get: function (key) {
+    return this.keys[key]
+  }
+}
+
+describe('Fetch REST endpoint', () => {
+  const app = express()
+  let server
+
+  before(() => {
+    app.get('/test-endpoint', (req, res) => res.send('HOORAY'))
+    app.get('/no-content', (req, res) => res.status(204).send())
+    app.get('/server-error', (req, res) => res.status(500).send('Borked'))
+    server = app.listen(
+      3003,
+      () => { console.log('Listening ... ') }
+    )
+  })
+
+  const tests = [
+    {
+      title: 'Content',
+      config: {
+        namespace: 'test',
+        templateUrlRegistryKey: 'url'
+      },
+      success: body => expect(body).to.eql('HOORAY')
+    },
+    {
+      title: 'No-content',
+      config: {
+        namespace: 'test',
+        templateUrlRegistryKey: 'nocontent'
+      },
+      success: body => expect(body).to.eql([])
+    },
+    {
+      title: 'Not found',
+      config: {
+        namespace: 'test',
+        templateUrlRegistryKey: 'notfound'
+      },
+      failure: err => {
+        expect(err.statusCode).to.eql(404)
+      }
+    },
+    {
+      title: 'Server error',
+      config: {
+        namespace: 'test',
+        templateUrlRegistryKey: 'servererror'
+      },
+      failure: err => {
+        expect(err.statusCode).to.eql(500)
+      }
+    }
+  ]
+
+  for (const test of tests) {
+    describe(`fetch ${test.title}`, () => {
+      const testEnv = {
+        bootedServices: { registry: mockRegistry }
+      }
+
+      const apiClient = new RestApiClient()
+
+      it('boot apiClient', (done) => {
+        apiClient.init(
+          test.config,
+          testEnv,
+          done
+        )
+      })
+
+      it('call api', (done) => {
+        apiClient.run(
+          {},
+          {
+            sendTaskSuccess: body => {
+              if (!test.success) {
+                return done('Expected API call to fail')
+              }
+              test.success(body)
+              done()
+            },
+            sendTaskFailure: err => {
+              if (!test.failure) {
+                return done(err)
+              }
+              try {
+                test.failure(err)
+                done()
+              } catch (ex) {
+                done(ex)
+              }
+            }
+          }
+        )
+      }) // it('call api' ...
+    }) // describe ...
+  } // for ...
+
+  after(() => {
+    server.close()
+  })
+})

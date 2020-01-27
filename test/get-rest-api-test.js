89bae6fda4b6efe35cfb30a2fef45640df0fedde
Meena.Hoda <meena.hoda@wmfs.net>
2020-01-27 13:38:53 +0000

    style: standard --fix

--- test/get-rest-api-test.js
+++ test/get-rest-api-test.js
@@ -6,10 +6,10 @@ const RestApiClient = require('./../lib/components/state-resources/get-data-from
 
 const mockRegistry = {
   keys: {
-    'test_url': 'http://localhost:3003/test-endpoint',
-    'test_nocontent': 'http://localhost:3003/no-content',
-    'test_notfound': 'http://localhost:3003/not-found',
-    'test_servererror': 'http://localhost:3003/server-error'
+    test_url: 'http://localhost:3003/test-endpoint',
+    test_nocontent: 'http://localhost:3003/no-content',
+    test_notfound: 'http://localhost:3003/not-found',
+    test_servererror: 'http://localhost:3003/server-error'
   },
 
   get: function (key) {
